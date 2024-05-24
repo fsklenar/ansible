5b67482d5259c38fb166ada7b963aa7df86e93b0
Frantisek Sklenar <frantisek.sklenar@gmail.com>
2024-05-24 11:08:07 +0200

    ansible

--- k8s-vms/scripts/do-port-forward.sh
+++ k8s-vms/scripts/do-port-forward.sh
@@ -9,12 +9,38 @@ function do_port_forward {
   port_forward_exists=`ps -ef | grep "$dest_ip:$2:$local_ip:$1" | grep -v grep`
   #do port_forward only when there is still not an active port_forward for source port
   if [[ -z "$port_forward_exists" ]]; then
-    logger "DO-PORT-FORWARD: Port forward for $1 port"
+    logger "DO-PORT-FORWARD: Port forward for source_port=$1, dest_port=$2"
     ssh -N -R $dest_ip:$2:$local_ip:$1 $public_server &
   fi
 }
 
-#Prometheus
-do_port_forward 9090 9090
+
+#params: namespace, app_name (label), source_port, dest_port
+function k8s_port_forward {
+  local TMP_POD
+  TMP_POD=""
+  while [ -z "${TMP_POD}" ]; do
+    TMP_POD="$(kubectl get pods --namespace $1 -l "app.kubernetes.io/name=$2" --field-selector=status.phase==Running --output jsonpath="{.items[0].metadata.name}")"
+    #echo "pod=${TMP_POD}"
+    sleep 10
+  done
+
+  sleep 20  #delay - to be sure pod is running
+
+  logger "K8S-PORT-FORWARD: Port forward for pod=${TMP_POD}, namespace=$1, app=$2, source_port=$3, dest_port=$4"
+
+  kubectl port-forward -n $1 ${TMP_POD} $3:$4 &
+
+  sleep 5
+
+  do_port_forward $3 $4
+}
+
+
+
 #Grafana
-do_port_forward 3000 3000
+k8s_port_forward prometheus-kps grafana 3000 3000
+
+#Prometheus
+k8s_port_forward prometheus-kps prometheus 9090 9090
+
