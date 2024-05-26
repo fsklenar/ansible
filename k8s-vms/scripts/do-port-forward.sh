19d65dea4d851c821e976aca4b6015572be7c1cb
Frantisek Sklenar <frantisek.sklenar@gmail.com>
2024-05-26 16:35:12 +0200

    ansible

--- k8s-vms/scripts/do-port-forward.sh
+++ k8s-vms/scripts/do-port-forward.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-local_ip="127.0.0.1"
 dest_ip="10.14.0.5"
 public_server="salauser@linuxadmin.sk"
 
 #params: source_port, dest_port
 function do_port_forward {
   local port
-  port_forward_exists=`ps -ef | grep "$dest_ip:$2:$local_ip:$1" | grep -v grep`
+  port_forward_exists=`ps -ef | grep "$dest_ip:$2:$3:$1" | grep -v grep`
   #do port_forward only when there is still not an active port_forward for source port
   if [[ -z "$port_forward_exists" ]]; then
     logger "DO-PORT-FORWARD: Port forward for source_port=$1, dest_port=$2"
-    ssh -N -R $dest_ip:$2:$local_ip:$1 $public_server &
+    ssh -N -R $dest_ip:$2:$3:$1 $public_server &
   fi
 }
 
 
-#params: namespace, app_name (label), source_port, dest_port
+#params: namespace, app_name (label), source_port, dest_port, local_app_ip, kubect_portfward (true/false)
 function k8s_port_forward {
   local TMP_POD
   TMP_POD=""
@@ -29,20 +28,20 @@ function k8s_port_forward {
 
   logger "K8S-PORT-FORWARD: Port forward for pod=${TMP_POD}, namespace=$1, app=$2, source_port=$3, dest_port=$4"
 
-  if [ $5 == "true" ]; then
+  if [ $6 == "true" ]; then #not needed if ingress is in place
     kubectl port-forward -n $1 ${TMP_POD} $3:$4 &
   fi
 
   sleep 5
 
-  do_port_forward $3 $4
+  do_port_forward $3 $4 $5
 }
 
 
 
 #Grafana
-k8s_port_forward monitoring grafana 3000 3000 true
+k8s_port_forward monitoring grafana 3000 3000 "10.192.168.202" false
 
 #Prometheus
-k8s_port_forward monitoring prometheus 9090 9090 false
+k8s_port_forward monitoring prometheus 9090 9090 "10.192.168.202" false
 
